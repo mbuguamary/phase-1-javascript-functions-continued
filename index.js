74717147f2eaa7e0066af82b90e53e2c746253c3
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-08-20 14:03:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate"){
+   return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+function mondayWork(activity="go to the office"){
+ return `This Monday, I will ${activity}.`
+}
+mondayWork()
+mondayWork("work from home")
+
+function wrapAdjective(star="*"){
+    return function(task="a hard worker"){
+        return `You are ${star}${task}${star}!`
+    }
+}
+wrapAdjective()()
+wrapAdjective("||")("a dedicated programmer")
